87f5f53d15636e8a88a02f458be73dd2cfb93954
Anand <anandnatarajanbtech@gmail.com>
2021-07-04 01:14:22 +0530

    geolocation and geocoder added

--- src/app/pages/geolocation/geolocation.page.ts
+++ src/app/pages/geolocation/geolocation.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Geolocation } from '@ionic-native/geolocation/ngx';
+import { NativeGeocoder, NativeGeocoderOptions, NativeGeocoderResult } from '@ionic-native/native-geocoder/ngx';
 
 @Component({
   selector: 'app-geolocation',
@@ -6,10 +8,71 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./geolocation.page.scss'],
 })
 export class GeolocationPage implements OnInit {
+  // Readable Address
+  address: string;
 
-  constructor() { }
+  // Location coordinates
+  latitude: number;
+  longitude: number;
+  accuracy: number;
+  pincode: number;
+
+
+  constructor(
+    private geolocation: Geolocation,
+    private nativeGeocoder: NativeGeocoder
+  ) { }
 
   ngOnInit() {
+  }
+
+
+  //Get current coordinates of device
+  getGeolocation() {
+    this.geolocation.getCurrentPosition().then((resp) => {
+      console.log(resp.coords)
+      this.latitude = resp.coords.latitude;
+      this.longitude = resp.coords.longitude;
+      this.accuracy = resp.coords.accuracy;
+
+      this.getGeoencoder(resp.coords.latitude, resp.coords.longitude);
+
+    }).catch((error) => {
+      console.log('Error getting location', error);
+    });
+  }
+
+  //geocoder method to fetch address from coordinates passed as arguments
+  getGeoencoder(latitude, longitude) {
+    //Geocoder configuration
+    let geoencoderOptions: NativeGeocoderOptions = {
+      useLocale: true,
+      maxResults: 5
+    };
+    this.nativeGeocoder.reverseGeocode(latitude, longitude, geoencoderOptions)
+      .then((result: NativeGeocoderResult[]) => {
+        this.address = this.generateAddress(result[0]);
+      })
+      .catch((error: any) => {
+        console.log('Error getting location', error);
+      });
+  }
+
+  //Return Comma saperated address
+
+  generateAddress(addressObj) {
+    let obj = [];
+    let address = "";
+    for (let key in addressObj) {
+      this.pincode = addressObj.postalCode;
+      obj.push(addressObj[key]);
+    }
+    obj.reverse();
+    for (let val in obj) {
+      if (obj[val].length)
+        address += obj[val] + ', ';
+    }
+    return address.slice(0, -2);
   }
 
 }
